5480d027c83f913e54ce94b4e10547cb67f3aa42
Mais <59611735+itsmais@users.noreply.github.com>
2020-10-12 22:58:30 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,13 @@
+var myHeaders = new Headers();
+myHeaders.append("Access-Control-Allow-Origin", "*");
+
 var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  
-  fetch("https://www.electoral-vote.com/evp2020/Pres/pres_polls.txt", requestOptions)
-    .then(response => response.text())
-    .then(result => {
-        console.log(result)
-    })
-    .catch(error => console.log('error', error));
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("https://www.electoral-vote.com/evp2020/Pres/pres_polls.txt", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
