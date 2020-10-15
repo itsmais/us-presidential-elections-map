58f28678a20a44f85bba21afbe530e76bcf54490
Mais <59611735+itsmais@users.noreply.github.com>
2020-10-15 10:56:38 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -695,7 +695,7 @@ let US_Sates_full = [
   'Iowa','Idaho','Illinois','Indiana','Kansas','Kentucky',
   'Louisiana','Massachusetts','Maryland','Maine','Michigan','Minnesota',
   'Missouri','Mississippi','Montana','North Carolina','North Dakota','Nebraska',
-  'New Hampshire','New Jersey','Nevada','New Mexico','New York','Ohio',
+  'New Hampshire','New Jersey','New Mexico','Nevada','New York','Ohio',
   'Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota',
   'Tennessee','Texas','Utah','Virginia','Vermont','Washington',
   'Wisconsin','West Virginia','Wyoming'
